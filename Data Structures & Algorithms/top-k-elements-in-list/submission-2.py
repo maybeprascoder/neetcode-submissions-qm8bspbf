f466ee969a3e2667ebc12b2c9c5e4c1c7391e8e6
maybeprascoder <kprasoon125@gmail.com>
2026-04-06 19:46:46 -0500

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        noof = {}
+        for num in nums:
+            noof[num] = noof.get(num,0) + 1
+        list1 = []
+        while(k):
+            res = max(noof, key=noof.get)
+            list1.append(res)
+            noof.pop(res)
+            k-=1
+
+        return list1
+        
